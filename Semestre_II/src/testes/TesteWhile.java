f35aa61bac46f888e25a88eecf0e588d10768d05
Breno A. Santos <breno_sant08@hotmail.com>
2018-08-31 17:56:13 -0300

    Teste com o while

---
+++ Semestre_II/src/testes/TesteWhile.java
@@ -0,0 +1,16 @@
+package testes;
+
+public class TesteWhile {
+
+	public static void main(String[] args) {
+		
+		
+		int count = 0;
+		
+		while( count <= 10 ) {
+			System.out.println("True");
+		}
+
+	}
+
+}
