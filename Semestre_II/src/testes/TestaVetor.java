78b1c8e7e8ccca5d909a6798ea69c4019bf63800
Breno A. Santos <breno_sant08@hotmail.com>
2018-09-04 12:39:35 -0300

    Testes com funções

--- Semestre_II/src/testes/TestaVetor.java
+++ Semestre_II/src/testes/TestaVetor.java
@@ -1,41 +1,112 @@
 package testes;
 
+import java.util.Scanner;
+
 public class TestaVetor {
+	
+	static Scanner in = new Scanner(System.in);
 
-	public static void main(String[] args) {
+	// Altera o valor default de cada posicao do vetor
+	public static boolean alteraValorDefault(int vetor[]) {
 		
-		final int arraySize = 5;
-		int[] array = new int[ arraySize ];
-		int emptyCount = 0;
+		int emptyPos = 0;
+		boolean vetorPreenchido = false;
 		
-		for( int i = 0; i < array.length; i++ )
-		{
-			array[ i ] = -1;
-		}
-		
-		for( int i = 0; i < array.length; i++ )
-		{
-			if( array[ i ] == -1 )
-				emptyCount++;
-
-		}
-		
-		System.out.println(emptyCount);
-		System.out.println(array.length);
-		
-		if( emptyCount == array.length)
-		{
-			System.out.println("Vetor vazio");
-		} else
-		{
-			System.out.println("Vetor ocupdado");
-		}
+		for( int i = 0; i < vetor.length; i++ ) {
 			
-		for( int j = 0; j < array.length; j++ )
-		{
-			System.out.println(array[ j ]);
+			vetor[ i ] = -1;
+			emptyPos++;
+			
 		}
-
+		
+		if( emptyPos != 0 ) {
+			vetorPreenchido = true;
+		}
+		
+		return vetorPreenchido;
+		
+	} // fim preencherVetor()
+	
+	
+	// Preenche o vetor com dados do usuario
+	public static void preencheVetor(int vetor[]) {
+		
+		for( int i = 0; i < vetor.length; i++ ) {
+			
+			System.out.print((i + 1) + " Valor: ");
+			vetor[ i ] = in.nextInt();
+		}
+		
+	} // fim preencheVetor()
+	
+	
+	// Ordena os dados do vetor usando o algoritmo de Selecao Direta
+	public static void ordenaDados(int vetor[]) {
+		
+		int indexAtual;
+		int valorAtual;
+		
+		for( int i = 0; i < vetor.length - 1; i++ ) {
+			
+			indexAtual = i;
+			valorAtual = vetor[ i ];
+			
+			for( int j = i + 1; j < vetor.length; j++ ) {
+				
+				if( vetor[ j ] < valorAtual ) {
+					
+					indexAtual = j;
+					valorAtual = vetor[ j ];
+					
+				} // fim do if
+				
+			} // fim do for interno
+			
+			vetor[ indexAtual ] = vetor[ i ];
+			vetor[ i ] = valorAtual;
+			
+		}
+		
+	} // fim ordenaDados
+	
+	
+	// Exibe os valores do vetor
+	public static void mostraValores(int vetor[]) {
+		
+		for( int i = 0; i < vetor.length; i++ ) {
+			
+			System.out.println("[" + i + "] >>> " + vetor[ i ]);
+			
+		} // fim do for
+		
+	} // fim mostraValores()
+	
+	
+	public static void main(String[] args) {
+	
+		int vetor[] = new int[ 5 ];
+		boolean statusVetor;
+		
+		statusVetor = alteraValorDefault(vetor);
+		
+		if( statusVetor == true ) {
+			
+			System.out.print("Vetor populado");
+			
+		} else {
+			
+			System.out.print("Vetor vazio");
+			
+		}
+		
+		System.out.println();
+		
+		preencheVetor(vetor);
+		
+		ordenaDados(vetor);
+		
+		mostraValores(vetor);
+		
 	}
 
 }
