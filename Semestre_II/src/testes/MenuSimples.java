2da4c8e8f32e154cb51b71eba0c5ae29d1d944aa
breno <breno@DESKTOP-I0QEU1T>
2018-09-04 00:12:33 -0300

    Sem alterações

--- Semestre_II/src/testes/MenuSimples.java
+++ Semestre_II/src/testes/MenuSimples.java
@@ -1,37 +1,66 @@
-package testes;
+ package testes;
 
 import java.util.Scanner;
 
-public class MenuSimples {
+public class MenuSimples{
 
 	// MENU
 	public static void menu() {
 		
 		System.out.println(); // Nova linha em branco
 		System.out.println("+================================+\n"
-						+ "||     1 - Cadastrar dados      ||\n"
-			 	  		+ "||     2 - Ordenar dados        ||\n"
-			 	  		+ "||     3 - Pesquisar dado       ||\n"
-			 	  		+ "||     4 - Exibir dados         ||\n"
-			 	  		+ "||     0 - Sair                 ||\n"
-			 	  		+ "+================================+\n"
-			 	  		+ "\nOpcao....: ");
+							  + "||     1 - Cadastrar dados      ||\n"
+							  + "||     2 - Ordenar dados        ||\n"
+							  + "||     3 - Pesquisar dado       ||\n"
+							  + "||     4 - Exibir dados         ||\n"
+							  + "||     0 - Sair                 ||\n"
+							  + "+================================+\n"
+							  + "\nOpcao....: ");
 		
-	}
+	} // fim de menu()
 	
 	public static void main(String[] args) {
 		
-		int opcao;
-		
 		Scanner in = new Scanner(System.in);
+		
+		char opcao;
 		
 		do {
 			
 			menu();
-			opcao = in.nextInt();
+			opcao = in.next().charAt(0);
 			
-		} while();
+			switch( opcao ) {
+			
+				case '1':
+			
+					fillArray(array);
+					break;
+				
+				case '2':
+				
+					sort(array);
+					break;
+				
+				case '3':
+				
+					// Your code is here
+					break;
+				
+				case '4':
+				
+					showData(array);
+					break;
+					
+				default :
+					
+					System.out.print("Opcao nao encontrada!");
+					break;
+			
+			}
+			
+		} while( opcao != '0' );
 
-	}
+	} // fim de main()
 
 }
